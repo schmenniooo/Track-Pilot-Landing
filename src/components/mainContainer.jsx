d059d17e02bf0217727c41ff41b5b3f427396114
ennio <ennio.nick@gmail.com>
2025-07-03 15:04:30 +0200

    emailjs integrated as email service

--- src/components/mainContainer.jsx
+++ src/components/mainContainer.jsx
@@ -1,34 +1,52 @@
 
-import './mainContainer.css'
+import './mainContainer.css';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 function MainContainer() {
 
-    const handleSubmit = (email) => {
+    const form = useRef(null);
 
-        // TODO: Write email to me with info. that customer with name clicked on submit
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            'service_xz2um1g',
+            'template_hx640wb',
+            form.current,
+            'nVmAr56Z9X6gZK7o5'
+        )
+            .then(() => {
+                alert("Thank you! You'll be notified.");
+                //form.current.reset();
+            })
+            .catch((error) => {
+                console.error('EmailJS Error:', error);
+                alert("Something went wrong. Try again.");
+            });
+    };
 
     return (
         <main className="mainContainer">
             <section className="logoContainer">
-                <img src="/logo.png" className="logo" alt="logo"/>
+                <img src="/logo.png" className="logo" alt="logo" />
             </section>
             <h1 className="heading">TrackPilot CRM</h1>
             <ul className="featureList">
                 <li>Easy to use CRM system for self employed people or small companies</li>
                 <li>Track your clients, orders and finance</li>
-                <li>Personalize your own experience with custom dashboard</li>
+                <li>Personalize your own experience with custom dashboards</li>
             </ul>
-            <form className="emailForm" onSubmit={handleSubmit}>
+            <form className="emailForm" ref={form} onSubmit={handleSubmit}>
                 <h3 className="formHeading">Get notified when we launch!</h3>
                 <p className="formSubtext">Be the first to know when TrackPilot CRM is ready.</p>
                 <div className="inputContainer">
-                    <input className="emailField" type="email" placeholder="Your email address" required />
+                    <input className="emailField" type="email" name="user_email" placeholder="Your email address" required/>
                     <button className="submitButton" type="submit">Notify Me</button>
                 </div>
             </form>
         </main>
-    )
+    );
 }
 
-export default MainContainer
+export default MainContainer;
